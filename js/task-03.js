89a44107a6c969ce590017eff791821c0a8f7e58
AnnaShkandrii <anna.didoshak@aiesec.net>
2023-05-11 16:49:00 +0300

    вправки

--- js/task-03.js
+++ js/task-03.js
@@ -13,7 +13,6 @@ const images = [
   },
 ];
 
-  
 const markup = images
   .map((image) => `<li class=gallery__item><img class=gallery__img src=${image.url} alt='${image.alt}' width='460'</img></li>`)
   .join("");
